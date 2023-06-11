dd8f8a0a9616a2c91d1b817304f9ff6faed374ba
SrinathSree <srinathsree122@gmail.com>
2023-06-11 16:07:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const div = document.querySelector('div');
 
 window.addEventListener('scroll', () => {
-    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement; // here we are getting properties from the documentElement
 
     const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
 
